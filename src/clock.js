b0ae7a382c690657ae833477bc034d15ecc03d34
Trevor Rapp <trrapp@gmail.com>
2018-01-03 18:17:42 -0700

    update to most recent version

--- src/clock.js
+++ src/clock.js
@@ -1,3 +1,7 @@
-window.onload = function connect () {
-	alert("javascript files are connected")
+function setDate () {
+	console.log("set date is getting called") 
 }
+
+setInterval
+// setInterval calls a function or evaluates an expression at specified intervals
+// The setDate() method sets the day of the Date object relative to the beginning of the currently set month.
